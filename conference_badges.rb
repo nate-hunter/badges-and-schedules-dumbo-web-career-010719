5950990a5be0f6d0c3602fbeb0672d8004b1c17c
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-21 14:25:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,12 +12,6 @@ def batch_badge_creator (speakers)
   end 
 end
 
-    it 'should return a list of welcome messages and room assignments' do
-      expect(assign_rooms(attendees)).to eq(room_assignments)
-    end
-    it 'should not hard-code the response' do
-      expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
-    end
 
 def assign_rooms (speakers)
   speakers.each_with_index do |name, room| #{speakers[room+1]!"
