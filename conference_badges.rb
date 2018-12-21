706a32229ed3b0932d6d9d0dddad970f1326c9c7
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-21 14:10:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,15 @@
 # Write your code here.
 
-
+speakers = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
 
 def badge_maker (name)
-  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
+def batch_badge_creator (speakers)
+  speakers.map do |name| 
+    "Hello, my name is #{name}."
+  end 
+end
+
+def assign_rooms S
